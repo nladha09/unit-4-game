5e406514cef77ed73420723248156ed57a568f5f
Noreen Ladha <ladha.noreen@gmail.com>
2018-11-11 10:26:38 -0700

    created initial files & added reset.css

---
+++ assets/sounds.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+var $hadoukenSound = $("#hadouken");
+
+	$(".character > .image").on('mouseenter', function() {
+		$hadoukenSound.get(0).play();
+	});
+
+	$(".character > .image").on('mouseout', function() {
+		$hadoukenSound.get(0).pause();
+		$hadoukenSound.get(0).currentTime = 0;
+	});
+
+var $enemySound = $('#enemySound');
+
+	$(".enemy > .image").on('mouseenter', function() {
+		$enemySound.get(0).play();
+	});
+
+	$(".enemy > .image").on('mouseout', function() {
+		$enemySound.get(0).pause();
+		$enemySound.get(0).currentTime = 0;
+	});
+
+});
